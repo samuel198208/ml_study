413d10898bf3e32a27093d003d3c919f4db6fdd0
Sangwoo Lee <neosw2@gmail.com>
2018-09-30 00:28:58 +0900

    sangwoo's implementation

--- sangwoo/sangwoo_python1.py
+++ sangwoo/sangwoo_python1.py
@@ -5,5 +5,12 @@ sys.stdin = open("input.txt", "r")
 T = int(input())
 for test_case in range(1, T + 1):
     # ///////////////////////////////////////////////////////////////////////////////////
-    
+    str = input()
+    ret = 0
+    cnt = [0 for _ in range(10)]
+    for i, ch in enumerate(str):
+        cnt[int(ch)] = (cnt[int(ch)] + 1) & 0x1
+    for i in range(10):
+        ret += cnt[i - 1]
+    print("#{} {}".format(test_case, ret))
     # ///////////////////////////////////////////////////////////////////////////////////
